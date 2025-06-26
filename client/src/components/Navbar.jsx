602ea0bf03c966e2f07c184cf3e7637b3eab1656
sanjanavatsavai <sanju82@outlook.com>
2025-06-26 14:16:58 -0400

    created the header section for app

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
     <div className='fixed top-0 left-0 z-50 w-full flex items-center
      justify-between px-6 md:px-16 lg:px-36 py-5'>
       <Link to='/' className='max-md:flex-1'>
-           <img src={assets.logo} alt="" className='w-36 h-auto'/> 
+           <img src={assets.logo} alt="" className='w-32 h-auto'/> 
       </Link>
 
       <div className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium
