f5bef7bea8f302c5276285d38b4f38111a6a8f25
sanjanavatsavai <sanju82@outlook.com>
2025-07-01 11:53:39 -0400

    seat layout to choose seats

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
       <Route path='/' element={<Home/>} />
       <Route path='/movies' element={<Movies/> } />
       <Route path='/movies/:id' element={<MovieDetails/> } /> 
-      <Route path='/movies/:id/:date' element={<SeatLayout/> } />
+      <Route path='/movies/:id/:date' element={<SeatLayout /> } />
       <Route path='/my-bookings' element={<MyBookings/> } />
       <Route path='/favorite' element={<Favorite/> } />
 
