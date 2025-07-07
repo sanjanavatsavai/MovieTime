233a9a0e6d84a67c42f3c9b241fdd5beb16933d0
sanjanavatsavai <sanju82@outlook.com>
2025-07-07 14:55:04 -0400

    updated db

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,24 +1,13 @@
-
 import mongoose from "mongoose";
 
-let isConnected = false; // track the connection state
-
 const connectDB = async () => {
-  if (isConnected) return;
-
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/movietime`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    isConnected = true;
-    console.log(" Database Connected");
-  } catch (error) {
-    console.error(" MongoDB connection error:", error.message);
-    throw error;
-  }
-};
+    try {
+        mongoose.connection.on('connected', () => console.log('Database Connected'));
+        await mongoose.connect(`${process.env.MONGODB_URI}/movietime`)
+        
+    } catch (error) {
+        console.log(error.message);
+    }
+}
 
 export default connectDB;
-
