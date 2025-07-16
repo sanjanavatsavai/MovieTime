8a38884c143349d30c0e52bef548cced3f09ea3d
sanjanavatsavai <sanju82@outlook.com>
2025-07-16 11:04:41 -0400

    api to get the seats and book

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "../server/inngest/index.js"
 import showRouter from './routes/showRoutes.js';
+import bookingRouter from './routes/bookingRoutes.js';
 
 
 const app = express();
@@ -27,5 +28,6 @@ app.use("/api/inngest", serve({ client: inngest, functions }))
 
 app.use('/api/show', showRouter)
 
+app.use('/api/booking', bookingRouter)
 
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
